7a91bb5b5aa736191a2a896eb2e6740306e0f092
Dan Lash <danlash@gmail.com>
2012-09-18 13:34:28 -0400

    fizz buzz with erin

---
+++ Source/Bowling.Specs/AssemblyInfoCommon.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("Debug")]
+[assembly: AssemblyProduct("TDD Bowling Game")]
+[assembly: AssemblyCopyright("Copyright 2012 VersionOne, Inc.")]
+[assembly: AssemblyVersion("0.0.1")]
+
---
+++ Source/Bowling/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+	public class FizzBuzz
+	{
+		public string For(int i)
+		{
+			var output = "";
+			if (i % 3 == 0) output = "fizz";
+			if (i % 5 == 0) output += "buzz";
+			return output;
+		}
+		public string[] UpTo(int lastInt)
+		{
+			var vals = new string[lastInt];
+			for (var i = 0; i < lastInt; i++)
+			{
+				vals[i] = this.For(i + 1);
+			}
+			return vals;
+		}
+	}
+}
