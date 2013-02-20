3f4e7820e3ea461fc75db13ba2d6f11163195b07
codeEmitter <greg.johnson@versionone.com>
2013-02-20 13:23:15 -0500

    Initial passing test for test case with 0

--- Source/Core.Specs/FizzBuzzSpecs.cs
+++ Source/Core.Specs/FizzBuzzSpecs.cs
@@ -4,4 +4,20 @@ using Main;
 
 namespace specs_for_fizz_buzz
 {
+    internal class when_fizzing_a_zero : concerns
+    {
+        private string _result;
+
+        protected override void context()
+        {
+            IFizzBuzz fizzer =new FizzBuzz();
+            _result = fizzer.For(0);
+        }
+
+        [Specification]
+        private void it_should_return_empty()
+        {
+            _result.should_equal("");
+        }
+    }
 }
--- Source/Core/IFizzBuzz.cs
+++ Source/Core/IFizzBuzz.cs
@@ -7,4 +7,26 @@ namespace Main
 		string For(int number);
 		IEnumerable<string> UpTo(int number);
 	}
+
+    public class FizzBuzz : IFizzBuzz
+    {
+        public FizzBuzz()
+        {
+            
+        }
+
+        private string _fbValue;
+        
+        public string For(int number)
+        {
+            _fbValue = number != 0 ? number.ToString() : "";
+
+            return _fbValue;
+        }
+
+        public IEnumerable<string> UpTo(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
