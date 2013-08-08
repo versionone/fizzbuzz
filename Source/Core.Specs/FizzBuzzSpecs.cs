4ddc6e00e892a6883683da0a82398b3324dcca8d
Dan Lash <danlash@gmail.com>
2013-08-08 13:29:58 -0400

    for divisible by 3 and one 5

--- Source/Core.Specs/FizzBuzzSpecs.cs
+++ Source/Core.Specs/FizzBuzzSpecs.cs
@@ -4,4 +4,78 @@ using Main;
 
 namespace specs_for_fizz_buzz
 {
+	public class when_fizzing_a_zero : concerns
+	{
+		private IFizzBuzz _fizzer;
+		private string _output;
+
+		protected override void context()
+		{
+			_fizzer = new FizzBuzz();
+			_output = _fizzer.For(0);
+		}
+
+		[Specification]
+		public void it_outputs_zero()
+		{ 
+			_output.should_equal("0");
+		}
+
+	}
+
+	public class when_fizzing_a_three : concerns
+	{
+		private IFizzBuzz _fizzer;
+		private string _output;
+
+		protected override void context()
+		{
+			_fizzer = new FizzBuzz();
+			_output = _fizzer.For(3);
+
+		}
+
+		[Specification]
+		public void it_outputs_fizz()
+		{
+			_output.should_equal("Fizz");
+		}
+	}
+
+	public class when_fizzing_a_five : concerns
+	{
+		private string _output;
+
+		protected override void context()
+		{
+
+			var fizzer = new FizzBuzz();
+			_output = fizzer.For(5);
+		}
+
+		[Specification]
+		public void it_outputs_buzz()
+		{
+			_output.should_equal("Buzz");
+		}
+	}
+
+	public class when_fizzing_a_nine : concerns
+	{
+		private string _output;
+
+		protected override void context()
+		{
+
+			var fizzer = new FizzBuzz();
+			_output = fizzer.For(9);
+		}
+
+		[Specification]
+		public void it_outputs_buzz()
+		{
+			_output.should_equal("Fizz");
+		}
+	}
+	
 }
---
+++ Source/Core/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Main;
+
+namespace Main
+{
+	public class FizzBuzz : IFizzBuzz
+	{
+		public string For(int number)
+		{
+			if (number > 0 && number % 3 == 0) return "Fizz";
+			if (number == 3) return "Fizz";
+			else if (number == 5) return "Buzz";
+			return number.ToString();
+		}
+
+		public IEnumerable<string> UpTo(int number)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
